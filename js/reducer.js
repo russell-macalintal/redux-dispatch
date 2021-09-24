4a0020c15f4593b61f511f9d745aadca95891d11
russell-macalintal <russell-macalintal@gmail.com>
2021-09-24 01:43:29 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,18 @@ function changeState(state, action){
   }
 }
 
+function dispatch(action){
+  state = changeState(state, action);
+  render();
+}
+
+function render(){
+  document.body.textContent = state.count;
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+dispatch(action);
+dispatch(action);
+dispatch(action);
